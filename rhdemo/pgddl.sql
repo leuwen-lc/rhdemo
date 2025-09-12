6e8bf955dc4cc16c76b4aa000bc1cf812c0c7d99
leno-vo <leno-vo@leno-vo-IdeaPad-Slim-3-15ABR8>
2025-09-12 19:07:38 +0200

    Evolution avec base PostgreSQL en run et H2 en test

---
+++ rhdemo/pgddl.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS employes;
+ 
+CREATE TABLE employes (
+  id BIGSERIAL PRIMARY KEY,  
+  prenom VARCHAR(250) NOT NULL,
+  nom VARCHAR(250) NOT NULL,
+  mail VARCHAR(250) NOT NULL,
+  mdp VARCHAR(250) NOT NULL
+);
+ 
+INSERT INTO employes (prenom, nom, mail, mdp) VALUES
+  ('Laurent', 'GINA', 'laurentgina@mail.com', 'laurent'),
+  ('Sophie', 'FONCEK', 'sophiefoncek@mail.com', 'sophie'),
+  ('Agathe', 'FEELING', 'agathefeeling@mail.com', 'agathe');
